6f4a299b240f0120ac9f6c4b5d2e3e80d1a38d93
Spek <gabrielallanlopws@gmail.com>
2025-04-21 12:50:42 -0300

    fix env var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://sistema-financeiro-api-iota.vercel.app/api'
+    apiUrl: 'https://sistema-financeiro-api1.vercel.app/api'
   };
   
